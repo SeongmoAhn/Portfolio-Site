31af9fa743f065429517704e19eaf010fb040c64
SeongmoAhn <asm0619@naver.com>
2026-09-03 16:48:51 +0900

    favicon 변경

---
+++ src/pages/Home/sections/Experience/Experience.tsx
@@ -0,0 +1,9 @@
+import styles from "./Experience.module.css";
+
+export default function Experience() {
+  return (
+    <section id="experience" className={styles.section}>
+      <h2>Experience</h2>
+    </section>
+  );
+}
