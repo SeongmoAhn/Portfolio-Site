1919dda1dccc5385466bf67e77fa41a3d7ad8c4e
SeongmoAhn <asm0619@naver.com>
2026-09-04 17:09:40 +0900

    About 레이아웃 기본

--- src/pages/Home/sections/About/About.tsx
+++ src/pages/Home/sections/About/About.tsx
@@ -3,27 +3,38 @@ import profileImg from '/src/assets/images/profile.png'
 
 export default function About() {
   return (
-    <section id="about" className={`${styles.section} section`}>
-      <h2>About</h2>
-      <p>안녕하세요.</p>
-      <p>본질에 집중하는 백엔드 개발자</p>
-      <p>안성모<span> 입니다.</span></p>
-      <div>
-        <p>학력</p>
-        <p>MIT 컴퓨터공학과 졸업</p>
+    <section id="about" className={`${styles.section} section container`}>
+      <h2 className={styles.category}>About</h2>
+
+      <div className={styles.profile}>
+        <div className={styles.content}>
+          <p className={styles.textSmall}>안녕하세요.</p>
+          <p className={styles.textBig}>본질에 집중하는 백엔드 개발자</p>
+          <p className={styles.textBig}>안성모<span className={styles.textSmall}> 입니다.</span></p>
+
+          <div>
+            <p className={styles.textSmall}>학력</p>
+            <p className={styles.text}>MIT 컴퓨터공학과 졸업</p>
+          </div>
+
+          <div>
+            <p className={styles.textSmall}>자격증</p>
+            <p className={styles.text}>외과 전문의 면허</p>
+            <p className={styles.text}>합기도 3단</p>
+            <p className={styles.text}>태권도 1단</p>
+            <p className={styles.text}>운전 면허 1종 보통</p>
+          </div>
+
+          <div>
+            <p className={styles.textSmall}>이메일</p>
+            <p className={styles.text}>asm0619@gmail.com</p>
+          </div>
+        </div>
+
+        <div className={styles.profileImage}>
+          <img src={profileImg} width="300" alt="프로필 이미지"/>
+        </div>
       </div>
-      <div>
-        <p>자격증</p>
-        <p>외과 전문의 면허</p>
-        <p>합기도 3단</p>
-        <p>태권도 1단</p>
-        <p>운전 면허 1종 보통</p>
-      </div>
-      <div>
-        <p>이메일</p>
-        <p>asm0619@gmail.com</p>
-      </div>
-      <img src={profileImg} width="300" alt="프로필 이미지"/>
     </section>
   );
 }
