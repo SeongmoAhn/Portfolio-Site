395690d6ef263c41f718810778f1d8d0d9266f32
SeongmoAhn <asm0619@naver.com>
2026-09-04 18:23:11 +0900

    로고 오타 수정 및 아이콘 추가

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -4,8 +4,11 @@ import styles from './Header.module.css'
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>Porfoilo</div>
-      <Navigation />
+      <div className={styles.logo}>
+        <img src='/public/favicon.svg' alt='favicon' width="30px"/>
+        <div>Portfoilo</div>
+      </div>
+      <Navigation/>
     </header>
   )
 }
