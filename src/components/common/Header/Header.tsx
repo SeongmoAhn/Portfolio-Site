8493c56e7955f317da28c8859a07ffa2ac9b2cd9
SeongmoAhn <asm0619@naver.com>
2026-09-06 21:54:18 +0900

    헤더 좌우여백 및 섹션 당 최소높이 추가

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,15 +1,17 @@
 import Navigation from "../Navigation/Navigation.tsx";
 import styles from './Header.module.css'
-import favicon from '../../../../public/favicon.svg'
+// import favicon from '../../../../public/favicon.svg'
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>
-        <img src={favicon} alt='favicon' width="30px"/>
-        <div>Portfoilo</div>
+      <div className={`${styles.inner} container`}>
+        <div className={styles.logo}>
+          {/*<img src={favicon} alt='favicon' width="30px"/>*/}
+          <div>Portfoilo</div>
+        </div>
+        <Navigation/>
       </div>
-      <Navigation/>
     </header>
   )
 }
